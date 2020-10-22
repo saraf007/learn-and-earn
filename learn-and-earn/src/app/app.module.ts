bfe33b9556df4453527db6fcbde6371909b4b1cd
Vikas Saraf <saraf.vikas5@gmail.com>
2020-10-22 18:00:06 +0530

    added question component

--- learn-and-earn/src/app/app.module.ts
+++ learn-and-earn/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ learn-and-earn/src/app/question/question.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-question',
+    templateUrl: './question.component.html'
+})
+export class QuestionComponent {
+
+}
