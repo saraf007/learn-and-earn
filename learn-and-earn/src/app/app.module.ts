685f7dd04f5258804e46495633eb04e2752f5334
Vikas Saraf <saraf.vikas5@gmail.com>
2020-10-24 18:53:52 +0530

    added UI for question and answer

--- learn-and-earn/src/app/app.module.ts
+++ learn-and-earn/src/app/app.module.ts
@@ -5,7 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Project
 import { QuestionComponent } from './question/question.component';
+
+// Angular Material
+import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,10 @@ import { QuestionComponent } from './question/question.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- learn-and-earn/src/app/question/question.component.ts
+++ learn-and-earn/src/app/question/question.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-question',
-    templateUrl: './question.component.html'
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.css']
 })
 export class QuestionComponent {
 
