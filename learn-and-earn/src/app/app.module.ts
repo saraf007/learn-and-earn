8f39583bf7b9d1807d8e18411dc39ab87c71a3a3
Vikas Saraf <saraf.vikas5@gmail.com>
2020-10-25 18:47:42 +0530

    added 2 way binding

--- learn-and-earn/src/app/app.module.ts
+++ learn-and-earn/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -24,7 +24,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserAnimationsModule,
     MatCardModule,
     MatRadioModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- learn-and-earn/src/app/question/question.component.ts
+++ learn-and-earn/src/app/question/question.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+// Project
+import { Question } from './questions.model';
 
 @Component({
     selector: 'app-question',
     templateUrl: './question.component.html',
     styleUrls: ['./question.component.css']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
+    answers: string[] = ["Option 1","Option 2","Option 3","Option 4"];
+    selectedAnswer: string;
+    question = "What is called the Power House of the Cell?";
 
+    constructor() {}
+
+    ngOnInit() {
+    }
+
+    onNext(answer: NgForm) {
+        console.log(answer);
+    }
+    
 }
